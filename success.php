9f84e9c2408e216a520eab3236d3001d4d55aa77
mignebulaconsulting <migenam@nebulaconsulting.co.uk>
2025-10-16 11:44:15 +0100

    success.php

---
+++ success.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+
+Welcome <?php echo $_POST["firstnamme"]; ?><br>
+Your email address is: <?php echo $_POST["email"]; ?>
+Your lastname: <?php echo $_POST["lastname"]; ?>
+
+</body>
+</html>
